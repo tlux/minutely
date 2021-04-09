8d294cf068d7b9d6e4bc684d86fc019f0c58e2a6
Tobias <tobias.casper@gmail.com>
2021-04-09 11:01:24 +0200

    docs: add method description to Utils#blank?

--- lib/day_time/utils.rb
+++ lib/day_time/utils.rb
@@ -6,6 +6,10 @@ module DayTime
   module Utils
     module_function
 
+    ##
+    # Determines whether the given value is nil or empty.
+    #
+    # @return [Boolean]
     def blank?(value)
       value.nil? || (value.respond_to?(:empty?) && value.empty?)
     end
